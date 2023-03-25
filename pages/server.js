74e09974c00b449b3be2e97a7938aad5a14d726b
r.chernetsov@adonis24.ru <40095266+Adonis24@users.noreply.github.com>
2023-03-25 18:41:25 +0300

    Update server.js
    Layout

--- pages/server.js
+++ pages/server.js
@@ -1,6 +1,6 @@
 import { unstable_getServerSession } from "next-auth/next"
 import { authOptions } from "./api/auth/[...nextauth]"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 
 import  { GetServerSidePropsContext } from "next"
 import  { Session } from "next-auth"
